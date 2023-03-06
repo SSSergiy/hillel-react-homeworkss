10bdedfbfdf13a45e9689373d7b4c4f74d7e86ff
SSSergiy <sergey1991serg@gmail.com>
2023-03-06 09:58:03 +0200

    add frame motion

--- hillel-react-homework-0007/src/pages/homePage/HomePage.tsx
+++ hillel-react-homework-0007/src/pages/homePage/HomePage.tsx
@@ -2,35 +2,81 @@ import { imgResursMasonry } from '../../imagesResurses/imgResursMasonry'
 import './_HomePage.scss'
 import { Masonry } from '@mui/lab'
 import { Button, ButtonGroup, Toolbar } from '@mui/material'
-import Box from '@mui/material/Box'
-import Paper from '@mui/material/Paper'
+import {Box,Paper} from '@mui/material'
+// import { motion } from "framer-motion";
 import { styled } from '@mui/material/styles'
 import { Link } from 'react-router-dom'
+import { motion, Variants } from "framer-motion";
+
+
+
+const cardVariants: Variants = {
+  offscreen: {
+    y: 300
+  },
+  onscreen: {
+    y: 50,
+    rotate: -10,
+    transition: {
+      type: "spring",
+      bounce: 0.4,
+      duration: 0.8
+    }
+  }
+};
+
+const hue = (h) => `hsl(${h}, 100%, 50%)`;
+
+function Card({ image}) {
+  const background = `linear-gradient(306deg, ${hue(79)}, ${hue(80)})`;
+
+  return (
+    <motion.div
+      className="card-container"
+      initial="offscreen"
+      whileInView="onscreen"
+      viewport={{ once: true, amount: 0.8 }}
+    >
+      <div className="splash" style={{ background }} />
+      <motion.div className="card" variants={cardVariants}>
+        <img src={image} alt="food" />
+      </motion.div>
+    </motion.div>
+  );
+}
 
 function HomePage() {
-	const Label = styled(Paper)(({ theme }) => ({
-		backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-		...theme.typography.body2,
-		padding: theme.spacing(0.5),
-		textAlign: 'center',
-		color: theme.palette.text.secondary,
-		borderBottomLeftRadius: 0,
-		borderBottomRightRadius: 0,
-	}))
+	// const Label = styled(Paper)(({ theme }) => ({
+	// 	backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+	// 	...theme.typography.body2,
+	// 	padding: theme.spacing(0.5),
+	// 	textAlign: 'center',
+	// 	color: theme.palette.text.secondary,
+	// 	borderBottomLeftRadius: 0,
+	// 	borderBottomRightRadius: 0,
+	// }))
 	return (
 		<div className="homePage">
 			<Toolbar className="toolbar">
-				<ButtonGroup variant="contained" className="buttongrup">
 					<Link to={'/login-page'}>
+					<motion.div
+      className="box"
+      whileHover={{ scale: 1.2 }}
+      whileTap={{ scale: 0.9 }}
+      transition={{ type: "spring", stiffness: 400, damping: 17 }}
+						>
+				<ButtonGroup variant="contained" className="buttongrup">
 						<Button color="success" className="button-LOG IN">
 							LOG IN
 						</Button>
-					</Link>
 				</ButtonGroup>
+							
+		</motion.div>
+					</Link>
 			</Toolbar>
 			<section>
 				<center>
-					<h1>star Wars</h1>
+					<h1>Star Wars</h1>
 				</center>
 				<ul>
 					<li>
@@ -70,7 +116,7 @@ function HomePage() {
 					</li>
 				</ul>
 			</section>
-			<Box sx={{ width: '100%', minHeight: 829, margin: '0 auto' }}>
+			{/* <Box sx={{ width: '100%', minHeight: 829, margin: '0 auto' }}>
 				<Masonry columns={3} spacing={2}>
 					{imgResursMasonry.map((item, index) => (
 						<div key={index}>
@@ -90,7 +136,11 @@ function HomePage() {
 						</div>
 					))}
 				</Masonry>
-			</Box>
+			</Box> */}
+{imgResursMasonry.map(({img},index) => (
+    <Card image={img}   key={index} />
+  ))}
+
 		</div>
 	)
 }
