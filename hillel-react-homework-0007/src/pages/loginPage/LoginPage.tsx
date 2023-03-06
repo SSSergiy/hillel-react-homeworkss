c14901e71349e4def18aeed4006c02bdf242598f
SSSergiy <sergey1991serg@gmail.com>
2023-03-06 10:37:06 +0200

    refactoring

--- hillel-react-homework-0007/src/pages/loginPage/LoginPage.tsx
+++ hillel-react-homework-0007/src/pages/loginPage/LoginPage.tsx
@@ -3,6 +3,9 @@ import './_LoginPage.scss'
 import { Button, ButtonGroup, Checkbox } from '@mui/material'
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { useRef } from "react";
+import { motion } from "framer-motion";
+import { useFollowPointer } from "./use-follow-pointer";
 
 function LoginPage() {
 	const [isChecked, setIsChecked] = useState(false)
@@ -10,9 +13,21 @@ function LoginPage() {
 	const handleCheckboxChange = (event) => {
 		setIsChecked(event.target.checked)
 	}
-
+	const ref = useRef(null);
+  const { x, y } = useFollowPointer(ref);
 	return (
 		<div className="homePages white">
+			 <motion.div
+      ref={ref}
+      className="box"
+      animate={{ x, y }}
+      transition={{
+        type: "spring",
+        damping: 3,
+        stiffness: 50,
+        restDelta: 0.001
+      }}
+    />
 			<BackButton />
 
 			<h1 className="white">Confirm Login</h1>
