627ec0c60026414e41a4624f5276c60a128c2b6b
SSSergiy <sergey1991serg@gmail.com>
2023-03-19 14:38:11 +0200

    oien

--- hillel-react-homework-0008/src/main.tsx
+++ hillel-react-homework-0008/src/main.tsx
@@ -4,15 +4,15 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
 import { StrictMode } from 'react';
-// import { createRoot } from 'react-dom/client';
-import { hydrateRoot } from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { App } from './App';
 import './index.css';
 import { store } from './store/store';
 
-hydrateRoot(
-  document.getElementById('root'),
+const root = createRoot(document.getElementById('root'));
+
+root.render(
   <StrictMode>
     <Provider store={store}>
       <App />
